2750fbc1342a28619b56a98107cbc4e16f10d69b
Tõnis Tiganik <tonisives@gmail.com>
2023-05-07 07:51:48 +0700

    Update vars.ts

--- packages/terraform/src/modules/vars.ts
+++ packages/terraform/src/modules/vars.ts
@@ -1,6 +1,8 @@
 import { Construct } from "constructs";
 import * as aws from '@cdktf/provider-aws';
 
+// add env vars directly to main.ts, into environment object for each lambda
+// This should be used if want to add variables to ssm
 export const addVars = (app: Construct) => {
 
 }
